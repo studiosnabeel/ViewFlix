e165ca3da45431e57eda14d4b69de96e94365d6e
Nabeel Akbar <studiosnabeel@gmail.com>
2023-04-15 05:27:58 +0500

    Custom hook and alot of components added

---
+++ src/pages/404/PageNotFound.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.scss';
+
+const PageNotFound = () => {
+  return <div>404</div>;
+};
+
+export default PageNotFound;
---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import './style.scss';
+import { useNavigate } from 'react-router-dom';
+import useFetch from '../../../hooks/useFetch';
+
+const HeroBanner = () => {
+  const [background, setBackground] = useState('');
+  const [query, setQuery] = useState('');
+  const navigate = useNavigate();
+
+  const { data, loading } = useFetch('/movie/upcoming');
+
+  useEffect(() => {
+    const bg = data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path
+  }, [data])
+
+  const searchQueryHandler = (event) => {
+    if (event.key === 'Enter' && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+
+  return (
+    <div className="heroBanner">
+      <div className="wrapper">
+        <div className="heroBannerContent">
+          <span className="title">Welcome.</span>
+          <span className="subTitle">
+            Millions of movies, TV shows and people to discover. Explore now.
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for movie or tv show..."
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchQueryHandler}
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
