f2f86c72aeafff4b923be536442ea284d47c4bea
Nabeel <nabeelakbar1983@gmail.com>
2023-04-17 02:29:52 +0500

    Add Components and scss

---
+++ src/components/contentWrapper/contentWrapper.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.scss";
+
+const contentWrapper = ({ children }) => {
+  return <div></div>;
+};
+
+export default contentWrapper;
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,31 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useSelector, userSelector } from 'react-redux';
-import './style.scss';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./style.scss";
 
-import useFetch from '../../../hooks/useFetch';
+import useFetch from "../../../hooks/useFetch";
+
+import Img from "../../../components/lazyLoadImage/img";
+import contentWrapper from "../../../components/contentWrapper/contentWrapper";
 
 const HeroBanner = () => {
-  const [background, setBackground] = useState('');
-  const [query, setQuery] = useState('');
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
   const navigate = useNavigate();
   const { url } = useSelector((state) => state.home);
-  const { data, loading } = useFetch('/movie/upcoming');
+  const { data, loading } = useFetch("/movie/upcoming");
 
   useEffect(() => {
-    const bg = data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
     setBackground(bg);
-  }, [data])
+  }, [data]);
 
   const searchQueryHandler = (event) => {
-    if (event.key === 'Enter' && query.length > 0) {
+    if (event.key === "Enter" && query.length > 0) {
       navigate(`/search/${query}`);
     }
   };
 
   return (
     <div className="heroBanner">
-      <div className="wrapper">
+      {!loading && (
+        <div className="backdrop-img">
+          <img src={background} />
+        </div>
+      )}
+      <contentWrapper>
         <div className="heroBannerContent">
           <span className="title">Welcome.</span>
           <span className="subTitle">
@@ -41,7 +51,7 @@ const HeroBanner = () => {
             <button>Search</button>
           </div>
         </div>
-      </div>
+      </contentWrapper>
     </div>
   );
 };
